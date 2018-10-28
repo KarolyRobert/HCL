9cd729abbdf844be543be0a9777862cfa171b492
RoGeR <karoly.robert.das@gmail.com>
2018-10-28 04:03:17 +0100

    fixing readme

--- hcl.js
+++ hcl.js
@@ -1,3 +1,25 @@
-/*
-  express middleware for handle http trafic
-*/
+const express = require("express");
+const Http_Crypto = require("./http_crypto");
+
+const app = express();
+var http_crypto = new Http_Crypto({
+  serverkey: "",
+  clientkey: ""
+});
+
+const port = 3000;
+
+app.use(http_crypto.middlaware);
+
+app.post("/", (req, res) => {
+  console.log("A bajött kérés:", req.json);
+  console.log(
+    req.headers["cf-connecting-ip"] ||
+      req.headers["x-forwarded-for"] ||
+      req.connection.remoteAddress
+  );
+  res.send("helo world phillipp");
+});
+app.post("/devices", (req, res) => res.send("devices"));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
